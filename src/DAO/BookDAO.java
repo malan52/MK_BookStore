ae3e96144a9b0f3eb8daaf59d95490ac275ae3f8
Roger Sun <rogeysun@gmail.com>
2016-03-16 14:35:57 -0400

    Changed all DAO classes.
    Changed the return type of all "retrieve" methods to corresponding Bean
    object instead of a Map object. Also changed names of the methods and
    added retrieveAll methods in BOOK, PO, and ADDRESS.

--- src/DAO/BookDAO.java
+++ src/DAO/BookDAO.java
@@ -22,12 +22,31 @@ public class BookDAO {
 		}
 	}
 
-	public Map<String, BookBean> retrieve(String bookID) throws SQLException {
+	public BookBean retrieveBook(String bookID) throws SQLException {
 		String query = "select B.BID, B.TITLE, B.PRICE, B.CATEGORY from BOOK B where B.BID = '" + bookID + "'";
-		Map<String, BookBean> rv = new HashMap<String, BookBean>();
 		Connection con = this.ds.getConnection();
 		PreparedStatement p = con.prepareStatement(query);
 		ResultSet r = p.executeQuery();
+		BookBean book = null;
+		while (r.next()) {
+			String bid = r.getString("BID");
+			String title = r.getString("TITLE");
+			int price = r.getInt("PRICE");
+			String category = r.getString("CATEGORY");
+			book = new BookBean(bid, title, price, category);
+		}
+		r.close();
+		p.close();
+		con.close();
+		return book;
+	}
+	
+	public Map<String, BookBean> retrieveAll() throws SQLException {
+		String query = "select * from BOOK";
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		Map<String, BookBean> rv = new HashMap<String, BookBean>();
 		while (r.next()) {
 			String bid = r.getString("BID");
 			String title = r.getString("TITLE");
