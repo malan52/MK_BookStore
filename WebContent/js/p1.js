dcff99d8035ff7f20136298a28991aa35ff80131
Evelyn <evelyn6574@gmail.com>
2016-03-23 01:11:56 -0400

    lalalala
    pick credit card part out of the payment page, change the total price of
    the cart.jspx, add js function to check the password for the register.

--- WebContent/js/p1.js
+++ WebContent/js/p1.js
@@ -1,15 +1,15 @@
- var city=[
+ var state=[
      ["Ontario","Quebec","Nova Scotia","New Brunswick","Manitoba","British Columbia","Prince Edward Island","Saskatchewan","Alberta","Newfoundland and Labrador" ],
      ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","Nevada","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio,Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","Washington","Wisconsin","Wyoming","District of Columbia"],
      ];
 
-     function getCity(){
+     function getState(){
          
-         var sltProvince=document.form1.province;        
-         var sltCity=document.form1.city;            
-         var provinceCity=city[sltProvince.selectedIndex - 1];         
-         sltCity.length=1;         
-         for(var i=0;i<provinceCity.length;i++){
-             sltCity[i+1]=new Option(provinceCity[i],provinceCity[i]);
+         var sltCountry=document.form1.country;        
+         var sltState=document.form1.state;            
+         var countryState=state[sltCountry.selectedIndex - 1];         
+         sltState.length=1;         
+         for(var i=0;i<countryState.length;i++){
+             sltState[i+1]=new Option(countryState[i],countryState[i]);
          }
      }
