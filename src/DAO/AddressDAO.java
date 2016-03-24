be7c22cc8ba24b524571cc9322db2031e34ed248
Malan <malan52.82@gmail.com>
2016-03-23 22:26:48 -0400

    update AddressBean and AddressDAO
    update billing and shipping into AddressBean and AddressDAO

--- src/DAO/AddressDAO.java
+++ src/DAO/AddressDAO.java
@@ -23,54 +23,101 @@ public class AddressDAO {
 		}
 	}
 
-	public AddressBean retrieveAddr(String addressID) throws SQLException {
-		String query = "SELECT * from ADDRESS A where A.ID = '" + addressID + "'";
+	/**
+	 * Return the only billing address of this customer
+	 * @param username
+	 * @return the only billing address of this customer
+	 * @throws SQLException
+	 */
+	public AddressBean retrieveBillingAddr(String username) throws SQLException {
+		String query = "SELECT * from ADDRESS where USERNAME = '" + username + "' AND TYPE = 'Billing'";//only one billing address for one user
 		AddressBean address = null;
 		Connection con = this.ds.getConnection();
 		PreparedStatement p = con.prepareStatement(query);
 		ResultSet r = p.executeQuery();
 		if (r.next()) {
-			String id = r.getString("ID");
+			//int id = r.getInt("ID");
+			String addrusername = r.getString("USERNAME");
 			String street = r.getString("STREET");
 			String province = r.getString("PROVINCE");
 			String country = r.getString("COUNTRY");
 			String zip = r.getString("ZIP");
 			String phone = r.getString("PHONE");
-			address = new AddressBean(id, street, province, country, zip, phone);
+			String type = r.getString("TYPE");
+			address = new AddressBean(addrusername, street, province, country, zip, phone, type);
 		}
 		r.close();
 		p.close();
 		con.close();
 		return address;
 	}
-
-	/*
-	 * Update a new customer' address or update an exist customer's address
+	
+	/**
+	 * Return an username's all past shipping address in a map with index
+	 * @param addressID
+	 * @return
+	 * @throws SQLException
 	 */
-	public void updateAddr(AddressBean addr) throws SQLException {
-		String queryget = "SELECT * FROM Address WHERE id = '" + addr.getId() + "'";
-		String queryadd = "INSERT INTO Address VALUES ('" + addr.getId() + "', '" + addr.getStreet() + "', '"
-				+ addr.getProvince() + "', '" + addr.getCountry() + "', '" + addr.getZip() + "', '" + addr.getPhone()
-				+ "')";
-		String queryupdate = "UPDATE Address SET \"STREET\"='" + addr.getStreet() + "', \"PROVINCE\"='"
-				+ addr.getProvince() + "', \"COUNTRY\"='" + addr.getCountry() + "', \"ZIP\"='" + addr.getZip()
-				+ "', \"PHONE\"='" + addr.getPhone() + "' WHERE \"ID\"='" + addr.getId() + "'";
+	public Map<Integer, AddressBean> retrieveShippingAddr(String username) throws SQLException {
+		String query = "SELECT * from ADDRESS where USERNAME = '" + username + "' AND TYPE = 'Shipping'";//one user may have more than one shipping address
+		Map<Integer, AddressBean> map = new HashMap<Integer, AddressBean>();
 		Connection con = this.ds.getConnection();
-		PreparedStatement p = con.prepareStatement(queryget);
+		PreparedStatement p = con.prepareStatement(query);
 		ResultSet r = p.executeQuery();
-		if (r.next()) { // user exist, update password
-			p = con.prepareStatement(queryupdate);
-			p.executeUpdate();
-		} else { // user not exist, insert new instance
-			p = con.prepareStatement(queryadd);
-			p.executeUpdate();
+		while (r.next()) {
+			int id = r.getInt("ID");
+			String addrusername = r.getString("USERNAME");
+			String street = r.getString("STREET");
+			String province = r.getString("PROVINCE");
+			String country = r.getString("COUNTRY");
+			String zip = r.getString("ZIP");
+			String phone = r.getString("PHONE");
+			String type = r.getString("TYPE");
+			map.put(id, new AddressBean(addrusername, street, province, country, zip, phone, type));
 		}
 		r.close();
 		p.close();
 		con.close();
+		return map;
+	}
+	
+	public AddressBean retrieveAddr(int addrID) throws SQLException {
+		String query = "SELECT * FROM Address WHERE ID = " + addrID;
+		AddressBean address = null;
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(query);
+		ResultSet r = p.executeQuery();
+		if (r.next()) {
+			String addrusername = r.getString("USERNAME");
+			String street = r.getString("STREET");
+			String province = r.getString("PROVINCE");
+			String country = r.getString("COUNTRY");
+			String zip = r.getString("ZIP");
+			String phone = r.getString("PHONE");
+			String type = r.getString("TYPE");
+			address = new AddressBean(addrusername, street, province, country, zip, phone, type);
+		}
+		r.close();
+		p.close();
+		con.close();
+		return address;
+	}
+	
+	/**
+	 * Update a new customer' address
+	 */
+	public void updateAddr(AddressBean addr) throws SQLException {
+		String queryadd = "INSERT INTO Address VALUES ('" + addr.getUsername() + "', '" + addr.getStreet() + "', '"
+				+ addr.getProvince() + "', '" + addr.getCountry() + "', '" + addr.getZip() + "', '" + addr.getPhone()
+				+ "', '" + addr.getType() + "')";
+		Connection con = this.ds.getConnection();
+		PreparedStatement p = con.prepareStatement(queryadd);
+		p.executeUpdate();
+		p.close();
+		con.close();
 	}
 
-	public Map<String, AddressBean> retrieveAll() throws SQLException {
+	/*public Map<String, AddressBean> retrieveAll() throws SQLException {
 		String query = "SELECT * from ADDRESS";
 		Map<String, AddressBean> rv = new HashMap<String, AddressBean>();
 		Connection con = this.ds.getConnection();
@@ -83,12 +130,13 @@ public class AddressDAO {
 			String country = r.getString("COUNTRY");
 			String zip = r.getString("ZIP");
 			String phone = r.getString("PHONE");
-			AddressBean address = new AddressBean(id, street, province, country, zip, phone);
+			String type = r.getString("TYPE");
+			AddressBean address = new AddressBean(id, street, province, country, zip, phone, type);
 			rv.put(id, address);
 		}
 		r.close();
 		p.close();
 		con.close();
 		return rv;
-	}
+	}*/
 }
