aed6b88a348b5407b756c9a976d4b6aba81a8710
Roger Sun <rogeysun@gmail.com>
2016-03-24 20:48:15 -0400

    Login modal connected to database.

---
+++ WebContent/js/login.js
@@ -0,0 +1,16 @@
+/**
+ * 	For login.japx only.
+ */
+
+function varifyLogin(){
+    	var xhttp = new XMLHttpRequest();
+    	xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+			  alert(xhttp.responseText);
+			}
+	    		  
+	    }
+   	 xhttp.open("POST", "LoginAndRegister", true);
+   	 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   	 xhttp.send("username=" + $("#login_username").val() +"&password=" + $("#login_password").val());
+    }
